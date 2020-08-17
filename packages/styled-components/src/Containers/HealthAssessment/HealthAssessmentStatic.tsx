e844492b9ce5459b878195a98c10dd295888c9bf
David Gruar <davidgruar@gmail.com>
2020-08-17 11:15:32 +0100

    Extract HealthAssessmentPage component

--- packages/styled-components/src/Containers/HealthAssessment/HealthAssessmentStatic.tsx
+++ packages/styled-components/src/Containers/HealthAssessment/HealthAssessmentStatic.tsx
@@ -4,19 +4,14 @@ import VisibilitySensor from 'react-visibility-sensor';
 import styled from 'styled-components';
 import { traversalConclusionsGetRequest } from '@doctorlink/traversal-redux';
 import colors from '../../Theme/base/colors';
-import Risks from './Risks/Risks';
-import HealthAge from './HealthAge/HealthAge';
-import Wellbeing from './Wellbeing/Wellbeing';
-import ActionsAndNumbers from './ActionsAndNumbers/ActionsAndNumbers';
-import AdditionalInfo from './AdditionalInfo/AdditionalInfo';
 import {
   CarouselNav,
   CarouselContainer,
   Pip,
 } from '../../Components/CarouselNavigation';
 import NavigationButtons from '../../Components/NavigationButtons';
-import ComparisonReport from './ComparisonReport/ComparisonReport';
 import { useHRARoutes, HraRouteName } from '../../Hooks';
+import { HealthAssessmentPage } from './HealthAssessmentPage';
 
 const A = styled.a`
   cursor: pointer;
@@ -38,28 +33,6 @@ const BottomBar = styled.footer`
   background-color: #fff;
   box-shadow: 0 -1px 5px ${colors.grey200};
 `;
-
-const Sections: React.FC<{
-  currentRoute: string;
-  traversalId: string;
-}> = ({ currentRoute, traversalId }) => {
-  switch (currentRoute) {
-    case 'health-age':
-      return <HealthAge traversalId={traversalId} />;
-    case 'risks':
-      return <Risks traversalId={traversalId} />;
-    case 'wellbeing':
-      return <Wellbeing traversalId={traversalId} />;
-    case 'my-numbers':
-      return <ActionsAndNumbers />;
-    case 'comparison-report':
-      return <ComparisonReport traversal={traversalId} />;
-    case 'info':
-      return <AdditionalInfo />;
-    default:
-      return null;
-  }
-};
 
 const HealthAssessment: React.FC<{
   traversalId: string;
@@ -96,7 +69,10 @@ const HealthAssessment: React.FC<{
         onChange={(isVisible) => setIsVisible(isVisible)}
       >
         <Content>
-          <Sections currentRoute={currentRoute} traversalId={traversalId} />
+          <HealthAssessmentPage
+            route={currentRoute}
+            traversalId={traversalId}
+          />
         </Content>
       </VisibilitySensor>
       {visible && (
