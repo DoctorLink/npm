404db565d6e0565f096ce2c6112b40b40d2dbaf5
David Gruar <davidgruar@gmail.com>
2020-11-09 09:50:45 +0000

    Reinstate pseudo-element before modal, make margin themeable

--- packages/styled-components/src/ComponentModules/Modal/Wrap.tsx
+++ packages/styled-components/src/ComponentModules/Modal/Wrap.tsx
@@ -3,14 +3,13 @@ import styled from 'styled-components';
 export const Wrap = styled.div`
   box-sizing: border-box;
   text-align: center;
-  position: absolute;
+  position: fixed;
   width: 100%;
   height: 100%;
   left: 0;
   top: 0;
   padding: 0 8px;
   white-space: nowrap;
-  position: fixed;
   overflow-x: hidden;
   overflow-y: auto;
   z-index: 1;
@@ -18,7 +17,11 @@ export const Wrap = styled.div`
   animation-iteration-count: 2;
   zoom: 1;
   transform-style: preserve-3d;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+
+  ::before {
+    content: '';
+    display: inline-block;
+    height: 100%;
+    vertical-align: middle;
+  }
 `;
