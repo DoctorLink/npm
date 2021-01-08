46e0ea5ac1c02dc33e663c51a6e6d0bd86930978
David Gruar <davidgruar@gmail.com>
2021-01-08 17:21:21 +0000

    Port traversal-redux tests

--- packages/traversal-redux/test/redux.test.ts
+++
@@ -1,8 +0,0 @@
-import { modalReducer } from '../src/Reducers/Modal';
-import { closeModal } from '../src/Actions/Modal';
-
-const r = modalReducer(undefined, closeModal());
-
-test('Modal Reducer', () => {
-  expect(r).toBe(null);
-});
