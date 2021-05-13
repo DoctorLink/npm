c9bd32c4a945abb7e75e950b8da46f8ac32d9e3c
Ken <ken.wyatt@doctorlink.com>
2021-05-13 15:39:13 +0100

    Added more tests for chatReducer

--- packages/traversal-redux/test/Reducers/chatTraversalReducer.test.ts
+++ packages/traversal-redux/test/Reducers/chatTraversalReducer.test.ts
@@ -1,9 +1,12 @@
-import { ChatTraversalState } from '@doctorlink/traversal-core';
+import { ChatModel, ChatTraversalState } from '@doctorlink/traversal-core';
 import { AxiosError } from 'axios';
 import {
   chatTraversalPostRequest,
   chatTraversalGetRequest,
   serviceSagaError,
+  chatTraversalPostResponse,
+  chatTraversalRespondPostRequest,
+  chatTraversalRevisitPostRequest,
 } from '../../src/Actions';
 import { chatReducer } from '../../src/Reducers';
 
@@ -25,6 +28,50 @@ describe('Chat reducer', () => {
       const updatedState = chatReducer(undefined, action);
 
       expect(updatedState.loading).toBe(true);
+    });
+  });
+
+  describe('chatTraversalRespondPostRequest', () => {
+    const action = chatTraversalRespondPostRequest('abc', [
+      { nodeId: 1, questionId: 1, answerId: 1 },
+    ]);
+
+    test('should set loading', () => {
+      const updatedState = chatReducer({} as ChatTraversalState, action);
+
+      expect(updatedState.loading).toBe(true);
+    });
+  });
+
+  describe('chatTraversalRevisitPostRequest', () => {
+    const action = chatTraversalRevisitPostRequest('abc', {
+      assetId: 3,
+      algoId: 1,
+      nodeId: 2,
+    });
+
+    test('should set loading', () => {
+      const updatedState = chatReducer({} as ChatTraversalState, action);
+
+      expect(updatedState.loading).toBe(true);
+    });
+  });
+
+  describe('chatTraversalPostResponse', () => {
+    const traversal = { algoId: 42 } as ChatModel;
+    const action = chatTraversalPostResponse(traversal);
+    const state = { loading: true } as ChatTraversalState;
+
+    test('should set traversal properties', () => {
+      const updatedState = chatReducer(state, action);
+
+      expect(updatedState.algoId).toBe(42);
+    });
+
+    test('should set loading = false', () => {
+      const updatedState = chatReducer(state, action);
+
+      expect(updatedState.loading).toBe(false);
     });
   });
 
